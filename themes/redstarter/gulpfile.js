2e4e2b9f72c8387446343880c75654f96b5209e5
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-05-17 14:44:10 -0700

    init

---
+++ themes/redstarter/gulpfile.js
@@ -0,0 +1,84 @@
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync');
+const cssnano = require('gulp-cssnano');
+const eslint = require('gulp-eslint');
+const gulp = require('gulp');
+const prettyError = require('gulp-prettyerror');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const terser = require('gulp-terser');
+const babel = require('gulp-babel');
+
+// Create basic Gulp tasks
+
+gulp.task('sass', function() {
+  return gulp
+    .src('./sass/style.scss', { sourcemaps: true })
+    .pipe(sourcemaps.init())
+    .pipe(prettyError())
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions']
+      })
+    )
+    .pipe(gulp.dest('./'))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('lint', function() {
+  return gulp
+    .src(['./js/*.js'])
+    .pipe(prettyError())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task(
+  'scripts',
+  gulp.series('lint', function() {
+    return gulp
+      .src('./js/*.js')
+      .pipe(
+        babel({
+          presets: ['env']
+        })
+      )
+      .pipe(terser())
+      .pipe(
+        rename({
+          extname: '.min.js'
+        })
+      )
+      .pipe(gulp.dest('./build/js'));
+  })
+);
+
+// Set-up BrowserSync and watch
+
+gulp.task('browser-sync', function() {
+  const files = [
+    './build/css/*.css',
+    './build/js/*.js',
+    './*.php',
+    './**/*.php'
+  ];
+
+  browserSync.init(files, {
+    proxy: 'localhost:8888/inhabitent'
+  });
+
+  gulp.watch(files).on('change', browserSync.reload);
+});
+
+gulp.task('watch', function() {
+  gulp.watch('js/*.js', gulp.series('scripts'));
+  gulp.watch('sass/*.scss', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
---
+++ themes/redstarter/inc/extras.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Custom functions that act independently of the theme templates.
+ *
+ * @package RED_Starter_Theme
+ */
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function red_starter_body_classes( $classes ) {
+	// Adds a class of group-blog to blogs with more than 1 published author.
+	if ( is_multi_author() ) {
+		$classes[] = 'group-blog';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'red_starter_body_classes' );
+
+function inhabitent_remove_submenus() {
+    remove_submenu_page( 'themes.php', 'theme-editor.php' );
+    remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
+}
+add_action( 'admin_menu', 'inhabitent_remove_submenus', 110 );
+
+
+//custom login logo
+function inhabitent_login_logo() {
+	echo '<style type="text/css">                                                                   
+		h1 a { background-image:url('.get_stylesheet_directory_uri().'/assets/images/logos/inhabitent-logo-text-dark.svg) !important; 
+		background-height: auto; !important; background-width: 100%; !important; margin-left: 50%; !important background-size: contain;}                            
+	</style>';
+}
+add_action('login_head', 'inhabitent_login_logo');
+
+function inhabitent_login_url( $url ) {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'inhabitent_login_url' );
