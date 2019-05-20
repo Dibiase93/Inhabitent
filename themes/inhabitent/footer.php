2455529560d7fc3abb53790954b71997dd2f4e67
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-05-19 17:30:18 -0700

    font working, implemented into footer

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -10,26 +10,36 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="contact-info-container">
-					<h3>Contact Info</h3>
-					<p><i class="fas fa-envelope"></i><a href="mailto:info@inhabitent.com">info@inhabitent.com</a></p>
-					<p><i class="fas fa-phone-square"></i>778-456-7891</p>
-					<i class="fab fa-facebook-square"></i>
-					<i class="fab fa-twitter-square"></i>
-					<i class="fab fa-google-plus-square"></i>
-				</div>
-				<div class="business-hours-container">
-					<h3>Business Hours</h3>
-					<p>Monday-Friday: 9am to 5pm</p>
-					<p>Saturday: 10am to 2pm</p>
-					<p>Sunday: Closed</p>
-				</div>
-				<div class="inhabitent-footer-logo">
-					<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/inhabitent-logo-text.svg" alt="inhabitent camping supply co. logo">
-				</div>
-				<div class="copyright-section">
-					<p>copyright &copy; 2019 inhabitent</p>
-				</div>
+				<div class="footer-container">
+					<div class="contact-info-container">
+						<h3>Contact Info</h3>
+						<p><i class="fas fa-envelope"></i><a href="mailto:info@inhabitent.com">info@inhabitent.com</a></p>
+						<p><i class="fas fa-phone-square"></i>778-456-7891</p>
+						<span>
+						<i class="fab fa-facebook-square"></i>
+						</span>
+						<span>
+						<i class="fab fa-twitter-square"></i>
+						</span>
+						<span>
+						<i class="fab fa-google-plus-square"></i>
+						</span>
+					</div>
+					<div class="business-hours-container">
+						<h3>Business Hours</h3>
+						<p><span class="footer-bold">Monday-Friday:</span> 9am to 5pm</p>
+						<p><span class="footer-bold">Saturday:</span> 10am to 2pm</p>
+						<p><span class="footer-bold">Sunday:</span> Closed</p>
+					</div>
+					<div class="inhabitent-footer-logo">
+						<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/inhabitent-logo-text.svg" alt="inhabitent camping supply co. logo">
+					</div>
+					<div class="copyright-section">
+						<a href="#">
+					  		<p><span class="copyright_caps">copyright</span> &copy; 2019 <span class="copyright_caps">inhabitent<span></p>
+						</a>
+					</div>
+                </div>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
