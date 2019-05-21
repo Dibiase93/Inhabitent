f635acd353f480da6a51843902c10050340c54ca
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-05-21 09:05:01 -0700

    add header, add side bar

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -17,7 +17,9 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
+				<div class="header-container">
 				<div class="site-branding">
+					<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/inhabitent-logo-tent.svg" alt="inhabitent brand logo">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
@@ -26,6 +28,8 @@
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
+				</div>
 			</header><!-- #masthead -->
+		
 
 			<div id="content" class="site-content">
