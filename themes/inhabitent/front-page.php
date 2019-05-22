e03e748164810cc15f1923ba4e9b084208298b65
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-05-22 14:39:58 -0700

    commite before code along for shop stuff section on homepage

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -10,9 +10,10 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<section class="fp-journal">
-				<h2>Inhabitent Journal</h2>
+		
+		  <section class="fp-journal">
+			<h2>Inhabitent Journal</h2>
+			<div class="journal-entries">
 				<?php 
 				$args = array(  'post_type' => 'post',
 								 'numberposts' => '3', 
@@ -28,21 +29,21 @@ get_header(); ?>
 				<?php endif; ?>
 
 				<!-- post author and date -->
-					<div class = "fp-post-meta">
-						<?php echo get_the_date(); ?>
-						<?php echo get_comments_number(); ?> Comments
-					</div>
+				<div class = "fp-post-meta">
+					<?php echo get_the_date(); ?>
+					<?php echo get_comments_number(); ?> Comments
+				</div>
 
 				  <!-- post title -->
-                	<h3>
-						<a href="<?php the_permalink(); ?>"><h2><?php the_title()?></h2></a>
-						<a class="read-more-btn" href="<?php the_permalink(); ?>">Read More</a>
-					</h3>
+                <h3>
+					<a href="<?php the_permalink(); ?>"><h2><?php the_title()?></h2></a>
+					<a class="read-more-btn" href="<?php the_permalink(); ?>">Read More</a>
+				</h3>
 				
 				  <?php endforeach; wp_reset_postdata(); ?>
-				
-				
-			</section>
+
+			</div>
+		  </section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
