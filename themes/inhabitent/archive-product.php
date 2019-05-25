b5bb308ba4fee5b25ca4b732ed7f84bec6b9caa4
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-05-25 13:43:41 -0700

    add archive.scss file, positioned elements on shop page

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -36,33 +36,38 @@ get_header(); ?>
 		</section>
 			</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
+            <?php /* Start the Loop */ ?>
+            
+            <div class="article-container">
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <header class="entry-header">
+                     
+                    
                         <?php if ( has_post_thumbnail() ) : ?>
                             <?php the_post_thumbnail( 'large' ); ?>
                         <?php endif; ?>
-                
-                        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-                
+                        
+                        <div class="product-content">
+                            <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+                            <span class="entry-price"><?php echo CFS()->get( 'product_price' );?></span>
+                        </div>
+
                         <?php if ( 'post' === get_post_type() ) : ?>
                         <div class="entry-meta">
                             <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
                         </div><!-- .entry-meta -->
+            
+
                         <?php endif; ?>
-                    </header><!-- .entry-header -->
                 
-                    <div class="entry-content">
-                    <?php echo CFS()->get( 'product_price' ); ?>
-                    </div><!-- .entry-content -->
+
                 </article><!-- #post-## -->
 				
 
 			<?php endwhile; ?>
-
+            </div>
 			<?php the_posts_navigation(); ?>
 
 		<?php else : ?>
@@ -74,5 +79,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
